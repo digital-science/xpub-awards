00bf89ce275626fe2f81e700ef023d2b30e071aa
Jared Watts <j.watts@digital-science.com>
2019-04-25 13:51:10 +0100

    [Workflow] remove generic client references

--- packages/component-workflow/src/workflow.js
+++ packages/component-workflow/src/workflow.js
@@ -1,26 +1,13 @@
 const TaskHandlers = require('./task-handlers/index');
 const { Client } = require('camunda-external-task-client-js');
 const config = require('config');
-
-const CamSDK = require('camunda-bpm-sdk-js');
-const camClient = new CamSDK.Client({
-    mock: false,
-    apiUri: config.workflow.apiUri
-});
-
-exports.client = camClient;
-exports.taskService = new camClient.resource('task');
-exports.processDefinitionService = new camClient.resource('process-definition');
-exports.processInstanceService = new camClient.resource('process-instance');
+const logger = require('@pubsweet/logger');
 
 exports.server = function setupWorkflow(app) {
 
     const clientConfig = { baseUrl: config.get('workflow.apiUri') };
     const client = new Client(clientConfig);
 
-    console.log("Setup external task handlers");
+    logger.info("Workflow - setup external task handlers");
     TaskHandlers(client);
-
-    console.log("Setup server");
-    console.dir(config);
 };
