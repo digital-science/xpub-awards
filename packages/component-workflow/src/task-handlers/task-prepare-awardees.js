8f2d4f39ab6dddb845bb2b4bfa0e3dc08ddf8759
Jared Watts <j.watts@digital-science.com>
2019-04-25 13:42:18 +0100

    [External-Task] initial implementation of prepare-awardees
    Take a listing of awardees and generate the awardee acceptance instances. The identifiers from these created objects are then passed back into the business process engine instance to create the child workflows.

--- packages/component-workflow/src/task-handlers/task-prepare-awardees.js
+++ packages/component-workflow/src/task-handlers/task-prepare-awardees.js
@@ -1,23 +1,62 @@
 const { Variables } = require('camunda-external-task-client-js');
+const { models } = require('component-workflow-model/model');
+const { AwardSubmission, AwardeeAcceptance } = models;
+const logger = require('@pubsweet/logger');
 
 module.exports = function _setupPrepareAwardeesTask(client) {
 
     client.subscribe('prepare-awardees', async ({ task, taskService }) => {
 
-        console.log(`[External-Task] Prepare Awardees:`);
-        console.log(JSON.stringify(task, null, 4));
+        logger.info(`[External-Task] Prepare Awardees called for submission '${task.businessKey}'`);
 
-        // The award submission itself can be found via the business key.
-        // But, need to resolve the model instance somehow???
-        // Maybe a "processDefinitionKey" to model mapping???
+        const submissionId = task.businessKey;
+        if(!submissionId) {
+            // FIXME: may need to fail task here and report it
+            logger.error(`External Task (prepare-awardees): failed to process for submission due to missing business key (processInstanceId="${task.processInstanceId}")`);
+            return;
+        }
+
+        const submission = await AwardSubmission.find(submissionId, 'awardees');
+        const awardees = submission.awardees;
+
+        if(!awardees || !awardees.length) {
+            logger.error(`External Task (prepare-awardees): insufficient awardees were present in the award submission data set (submissionId="${submissionId}")`);
+            return;
+        }
+
+
+        // Iterate each of the awardees and create an award acceptance model instance.
+        const awardAcceptances = await Promise.all(awardees.map(async awardee => {
+
+            const acceptance = new AwardeeAcceptance({
+                created: new Date().toISOString(),
+                updated: new Date().toISOString(),
+            });
+
+            await acceptance.save();
+
+            // Link the awardee and award submission back to this award acceptance.
+            await Promise.all([
+                acceptance.$relatedQuery('awardee').relate(awardee.id),
+                acceptance.$relatedQuery('awardSubmission').relate(submissionId)
+            ]);
+
+            return acceptance;
+        }));
+
+        console.dir(awardAcceptances);
+
+        const acceptanceIds = awardAcceptances.map(a => a.id);
 
         const processVariables = new Variables();
-        processVariables.set("awardees", ["jared-watts", "simon-porter"]);
-
-        // Once we have a listing of the awardees, then we need to create a collection
-        // of model instances that represent the awardee acceptance process.
-        // ??? how is this defined ???
+        processVariables.set("awardees", acceptanceIds);
 
         await taskService.complete(task, processVariables);
+
+
+        // TODO: we want to be able to retrieve all of the award acceptances from the original award as well via the GraphQL endpoint
+        // does the awardee reference the acceptance instance or does the award submission instance allow for looking up all
+        // referenced acceptance instances ???
+
     });
 };
