2baf38d8f405d260551607af0a157402dd5a41de
Jared Watts <j.watts@digital-science.com>
2019-04-25 13:47:18 +0100

    [Model] cleanup ability to find dynamically registered models

--- packages/component-workflow-model/index.js
+++ packages/component-workflow-model/index.js
@@ -1,24 +1,12 @@
-console.log('[component-workflow-model] Loading');
-
-const workflowDesc = require('./../app/config/description');
-const generateGraphQLDefs = require('./src/generate-graphql-definitions');
-
-const defs = generateGraphQLDefs(workflowDesc);
-console.log(defs.typeDefs);
-
-const File = require('./shared-model/file');
-
+const WorkflowModel = require('./model');
 
 function serverSetup(app) {
-
-    // Add the file download endpoint to the server.
+    const File = require('./shared-model/file');
     File.serverSetup(app);
 }
 
-
 module.exports = {
-
     server: () => serverSetup,
-    typeDefs: defs.typeDefs,
-    resolvers: defs.resolvers
+    typeDefs: WorkflowModel.typeDefs,
+    resolvers: WorkflowModel.resolvers
 };
