470de7bb45beb46d0fd72ac6c46b79ff71a679b9
Jared Watts <j.watts@digital-science.com>
2019-05-02 10:58:35 +0100

    [Task/EmailAwardee] initial code to load awardee acceptance w/associated awardee

--- packages/component-workflow/src/task-handlers/task-email-awardee.js
+++ packages/component-workflow/src/task-handlers/task-email-awardee.js
@@ -1,9 +1,38 @@
+const { models } = require('component-workflow-model/model');
+const { AwardeeAcceptance } = models;
+
+
 module.exports = function _setupEmailAwardeeTask(client) {
+
     client.subscribe('email-awardee', async ({ task, taskService }) => {
+
+        console.log("---------------");
         console.log(`[External-Task] Email Awardee:`);
         console.log(JSON.stringify(task, null, 4));
 
-        /* const processID = task.processInstanceId; */
+        const acceptanceId = task.businessKey;
+        if(!acceptanceId) {
+            // FIXME: may need to fail task here and report it
+            logger.error(`External Task (email-awardee): failed to process for submission due to missing business key (processInstanceId="${task.processInstanceId}")`);
+            return;
+        }
+
+        const acceptance = await AwardeeAcceptance.find(acceptanceId, ['awardee', 'awardSubmission']);
+        if(!acceptance) {
+            logger.warn(`External Task (email-awardee): unable to find awardee acceptance instance for id (${acceptanceId})`);
+            return;
+        }
+
+        const submission = acceptance.awardSubmission;
+        const awardee = acceptance.awardee;
+
+        console.log("Would have sent an email to the following awardee, regarding the award submission: ");
+        console.dir(submission);
+        console.dir(awardee);
+        console.log("---------------");
+
+        // FIXME: record sending an email etc. in a communication log associated with the submission??
+
         await taskService.complete(task);
     });
 };
