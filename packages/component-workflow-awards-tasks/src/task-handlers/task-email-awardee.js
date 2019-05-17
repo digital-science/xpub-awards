4e2cb31b9b587b6c3b7df99c720bb0dbdb0238fd
Jared Watts <j.watts@digital-science.com>
2019-05-17 11:17:07 +0100

    [Awards Tasks] implement email awardee task using workflow email service

--- packages/component-workflow-awards-tasks/src/task-handlers/task-email-awardee.js
+++ packages/component-workflow-awards-tasks/src/task-handlers/task-email-awardee.js
@@ -1,38 +1,73 @@
 const { models } = require('component-workflow-model/model');
 const { AwardeeAcceptance } = models;
+const logger = require('@pubsweet/logger');
+const config = require('config');
+
+const { ServiceSendEmail, EmailTemplate } = require('component-workflow-send-email');
+
+
+const LogPrefix = '[ExternalTask/EmailAwardee]';
 
 
 module.exports = function _setupEmailAwardeeTask(client) {
 
+    const emailTemplate = new EmailTemplate("awardee-acceptance");
+    const baseUrl = config.get('pubsweet-client.baseUrl');
+
+    async function sendEmailForAwardeeAcceptance(acceptance) {
+
+        const submission = acceptance.awardSubmission;
+        const awardee = acceptance.awardee;
+        const awardAcceptanceUrl = `${baseUrl}/award/${acceptance.id}/acceptance`;
+
+        if(!awardee) {
+            logger.error(`${LogPrefix} no awardee present in award acceptance instance`);
+            return Promise.reject(new Error("No awardee present to send email to."));
+        }
+
+        if(!awardee.email) {
+            logger.error(`${LogPrefix} awardee has no valid email address to send an email to`);
+            return Promise.reject(new Error("Awardee has no valid email address."));
+        }
+
+        const text = emailTemplate.template({awardee, submission, awardAcceptanceUrl});
+        const to = (awardee.firstName && awardee.lastName && awardee.firstName.indexOf('@') === -1 && awardee.lastName.indexOf('@') === -1) ?
+            `${awardee.firstName} ${awardee.lastName} <${awardee.email}>` : `${awardee.email}`;
+
+        //awardee.email
+
+        const opts = {
+            subject: "Award Acceptance",
+            to,
+            text
+        };
+
+        return ServiceSendEmail.sendEmail(opts);
+    }
+
     client.subscribe('email-awardee', async ({ task, taskService }) => {
 
-        console.log("---------------");
-        console.log(`[External-Task] Email Awardee:`);
-        console.log(JSON.stringify(task, null, 4));
+        logger.debug(`${LogPrefix} email awardee is starting`);
 
         const acceptanceId = task.businessKey;
         if(!acceptanceId) {
             // FIXME: may need to fail task here and report it
-            logger.error(`External Task (email-awardee): failed to process for submission due to missing business key (processInstanceId="${task.processInstanceId}")`);
+            logger.error(`${LogPrefix} failed to process for submission due to missing business key (processInstanceId="${task.processInstanceId}")`);
             return;
         }
 
         const acceptance = await AwardeeAcceptance.find(acceptanceId, ['awardee', 'awardSubmission']);
         if(!acceptance) {
-            logger.warn(`External Task (email-awardee): unable to find awardee acceptance instance for id (${acceptanceId})`);
+            logger.warn(`${LogPrefix} unable to find awardee acceptance instance for id (${acceptanceId})`);
             return;
         }
 
-        const submission = acceptance.awardSubmission;
-        const awardee = acceptance.awardee;
+        sendEmailForAwardeeAcceptance(acceptance).then(() => {
 
-        console.log("Would have sent an email to the following awardee, regarding the award submission: ");
-        console.dir(submission);
-        console.dir(awardee);
-        console.log("---------------");
+            logger.debug(`${LogPrefix} email to awardee was sent, completing external task`);
+            return taskService.complete(task);
+        });
 
         // FIXME: record sending an email etc. in a communication log associated with the submission??
-
-        await taskService.complete(task);
     });
 };
